368b2f7452acb1b308336ef76083c345af0526a7
fjaviergb <fco.javier.gb1@gmail.com>
2021-04-26 13:04:53 +0200

    Final commit

--- receiving.py
+++ receiving.py
@@ -4,24 +4,22 @@ import zlib
 
 from iota.transaction.types import TransactionTrytes
 
-# IOTA CONF
+# IOTA CONF - INTRODUCE TAIL_TRANS
 api = Iota('https://nodes.thetangle.org:443')
-seed = 'WVVBOHNRYPONH9GW9BXVAKGGYBULIUPCDVTFSUAFFJP99NTJTGGQRWQCXSWELVDQIRFINIQDMULGEXSWN'
-address = ['WRIIHMSNYWKGRNRWLLPTANKPYAGHIOWKFYSWOPCZMQIEAFQVWJKVMOSOAFGVOOBGUJISAZFQAPFQXIO9D']
-tag = ['PRUEBADEPDF']
-bundle_hash = ['KVHLEYYVZAPQTKTECHAKBVEDNAFLNVMUDJEPYTLVFCZJNLJCEKAZQ9KHEQCLDPT9LXXYZGQCKEI9COEDC']
-tail_transaction = 'STSOUKPNZGAEFJTYLXTQBMLYR9JUYMWCRCYEXPTBBBGSSAOKVIODOYZFJTWKGYMIXJEANMQQKVKQ99999'
+tail_transaction = 'DLOXEGLGBWWLFYLPKEIWGHC9UMNJBQOUXJSXEWJTLWI9LNTGQRFNYGUMYNUTOCVLEMXQSAOVGRNIA9999'
 
+# ADJUSTING QUERY - CHANGE
+data_size = 5696
 bundle = Iota.get_bundles(api,tail_transaction)
-
 trytes = bundle['bundles'][0].as_json_compatible()
 total = TransactionTrytes(b'')
 for i in trytes:
     total += i['signature_message_fragment']
-file = TryteString.encode(total[2673:8369])
+file = TryteString.encode(total[2673:2673+data_size])
 
+# CREATING FILE
 decompressed = zlib.decompress(file)
 bPDFout=codecs.decode(decompressed, 'base64')
-datafile=open("C:\\Master\\CUARTO CUATRIMESTRE\\Comunicacion y Divulgacion\\trabajo\\1.txt",'wb')
+datafile=open("1.txt",'wb')
 datafile.write(bPDFout)
 datafile.close()
--- sending.py
+++ sending.py
@@ -1,59 +1,38 @@
 from iota import Iota, TryteString, ProposedTransaction, Address, Tag
-import random
 import codecs
 import zlib
 
+# IOTA API CONNECTION. 
 api = Iota('https://nodes.thetangle.org:443')
-
-# SEED CREATED BY PSEUDO-RAND IN PYTHON - NOT SECURE
-seed = 'WVVBOHNRYPONH9GW9BXVAKGGYBULIUPCDVTFSUAFFJP99NTJTGGQRWQCXSWELVDQIRFINIQDMULGEXSWN'
-address = ['WRIIHMSNYWKGRNRWLLPTANKPYAGHIOWKFYSWOPCZMQIEAFQVWJKVMOSOAFGVOOBGUJISAZFQAPFQXIO9D']
-tag = ['PRUEBADEPDFA']
-
+address = ['WRIIHMSNYATYRNRWLLPTANKPYAGHIOWKFYSWOPCZMQIEAFQVWJKVMOSOAFGVOOBGUJIBNVFQAPFQXIO9D']
+tag = ['DIVULGACION']
 
 #FILE 
-f = open("C:\\Master\\CUARTO CUATRIMESTRE\\Comunicacion y Divulgacion\\trabajo\\0.txt", "rb")
+f = open("0.txt", "rb")
 pdfdatab=f.read()
 f.close()
 
 #ENCODE
 b64PDF = codecs.encode(pdfdatab, 'base64')
-# Sb64PDF=b64PDF.decode('utf-8')
 
 #COMPRESS
 compressed = zlib.compress(b64PDF,9)
-print(len(compressed))
 
-#KEY - https://stackoverflow.com/questions/30056762/rsa-encryption-and-decryption-in-python
-# https://cryptography.io/en/latest/
-# https://stuvel.eu/python-rsa-doc/usage.html
-# (pubkey, privkey) = rsa.newkeys(2048)
-# crypto = rsa.encrypt(b64PDF, pubkey)
-# print(crypto)
-
-#LECTURA - https://stackoverflow.com/questions/56230203/converting-a-pdf-file-or-any-binary-to-a-string-in-python-not-grab-text-out-o
-#bretdata=Sb64PDF.encode('utf-8')
-decompressed = zlib.decompress(compressed)
-bPDFout=codecs.decode(b64PDF, 'base64')
-datafile=open("C:\\Master\\CUARTO CUATRIMESTRE\\Comunicacion y Divulgacion\\trabajo\\1.txt",'wb')
-datafile.write(bPDFout)
-datafile.close()
-
+# CREATE TRANSACTION
 message = [TryteString.from_bytes(compressed)]
-print(len(message[0]))
+print('Data size:', len(message[0]))
 
 tx = ProposedTransaction(
-    address = Address(random.choice(address)),
-    message = random.choice(message),
+    address = Address(address[0]),
+    message = message[0],
     value = 0,
-    tag = Tag(random.choice(tag))
+    tag = Tag(tag[0])
 )
 
 result = api.send_transfer(transfers=[tx])
 
-print('Transacción enviada correctamente',
-        result['bundle'].tail_transaction.hash,
-        result['bundle'].hash)
+print('Transacción enviada correctamente')
+print('Tail_transaction:', result['bundle'].tail_transaction.hash)
 
 
 
